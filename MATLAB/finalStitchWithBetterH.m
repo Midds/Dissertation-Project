38561b9f734f3e69f8aaeb356914e81c4d4e7dba
James Middleton <13389226@students.lincoln.ac.uk>
2017-05-08 04:16:30 +0100

    more cleanup

--- MATLAB/finalStitchWithBetterH.m
+++ MATLAB/finalStitchWithBetterH.m
@@ -2,32 +2,28 @@
 % TO RUN, VLFEAT MUST FIRST BE INSTALLED ON THE MACHINE
 % VLFEAT can be downloaded from http://www.vlfeat.org/download.html or http://www.vlfeat.org/index.html
 % (however, this should already be downloaded and placed in the current
-% folder, but the below command still needs to be executed)
-% Once downloaded and unpacked the command below must be ran on each Matlab restart:
-% " run MY_PATH\ImageStitching\MATLAB\vlfeat-0.9.20/toolbox/vl_setup "
-% - with the pathway changed to match the vl_setup path
-% for example:
-% " run D:\Users\James\Documents\GitHub\ImageStitching\MATLAB\vlfeat-0.9.20/toolbox/vl_setup "
-% C:\Users\Computing\Downloads\vl\vlfeat-0.9.20-bin.tar\vlfeat-0.9.20-bin\vlfeat-0.9.20\toolbox
+% folder, but the below command still needs to be executed on each Matlab restart)
+% "run vlfeat-0.9.20\toolbox\vl_setup"
 close all;
-
-% pipeline as follows
-% - read in images to mosaic
-% - find sift for each and check there's enough matches, if not read in
-% another image
-% - use ransac on sift matches to get homograpy
-% - refine homography using levenberg-marquardt
-% - stitch images
-
-%line 111 change threshold to 25 for barett images
 
 % select number of images to stitch
 numToStitch = 7;
 % select the starting image to stitch (whatever number it is in the file)
 % eg if image name = 'im168.jpeg', then startImage = 168
 startImage = 168;
+
 % Creating an array to store the images
 imArray = {};
+
+% Threshold for Euclidean distance (for finding matches between SIFT descriptors)
+% Higher threshold (50+) recommended for images with low match % (barret images)
+euclideanThresh = 55;
+
+% modifying numToStitch to be compatible with current implementation
+% (needs to be an odd number)
+if mod(numToStitch, 2) == 0
+    numToStitch = numToStitch - 1;
+end
 
 for n = startImage:(startImage+numToStitch)-1
     filename = sprintf('barret1/im%d.jpeg', n); % defining the filename
@@ -36,11 +32,11 @@ for n = startImage:(startImage+numToStitch)-1
     imArray = [imArray im]; % adding the image to the image Array
 end
 
-% showing images to be stitched
-% figure;
-% newimage = cell2mat(imArray);
-% imshow(newimage);
-% title('Images to stitch');
+ showing images to be stitched
+ figure;
+ newimage = cell2mat(imArray);
+ imshow(newimage);
+ title('Images to stitch');
 
 % Read the first image from the image set.
 im1 = imArray{1};
@@ -51,7 +47,6 @@ im1 = im2single(im1);
     if size(im1,3) > 1 
         Ig = rgb2gray(im1); 
         Ig = imadjust(Ig);
-
     else
         Ig = im1;
     end
@@ -66,11 +61,11 @@ disp('sift features found for image: 1');
 %[features, points] = extractFeatures(grayImage, points);
 
 % Now loop through the remaining images
-% The nested loop will save a homography matrix (Hn_n+1) for each successive image
+% The nested loop will save a homography matrix (Hn) for each successive image
 % pair up until the centre image of the image array. The image array is
 % then reversed in order, and the nested loop will go again, looping until
-% it hits the centre image. This is needed as the H matrices need to be in
-% this order to successfully stitch them together later.
+% it hits the centre image again. This is needed as the H matrices need to be created 
+% in this direction to successfully stitch them together later.
 m = 2;
 for j = 1:2
     for n = 2:(numToStitch/2) + 1
@@ -92,7 +87,6 @@ for j = 1:2
         if size(I,3) > 1
             Ig = rgb2gray(I);
             Ig = imadjust(Ig);
-
         else
             Ig = I;
         end
@@ -101,103 +95,110 @@ for j = 1:2
         fprintf('');
         
         %% FINDING SIFT FEATURES AND DESCRIPTORS
-        
+        % returns keypoints and descriptors for image Ig
         [F2,D2] = vl_sift(Ig);
         
         fprintf('sift features found for image: %d\n', n);
-        
+        %% MATCHING DESCRIPTORS
+        % Nearest neighbour between two sets of descriptors will find the
+        % closest descriptor match.
         d = dist(D1',D2); % Distance between D1's column and D2's column
-    [Y I] = min(d);
-    count = 0; % Number of non-overlapped correspondences
-    c1 = zeros(1,2); % Corresponding feature coordinates of im1
-    c2 = zeros(1,2); % Corresponding feature coordinates of im2
-    %% Find correspondences between two images using Euclidean distance %%
-    img = [imArray{n-1},imArray{n}];
-    for k = 1:length(Y)
-        ind = 1; % Indicator to avoid overlapped correspondences
-        for l = 1:length(I)
-            if l~=k && I(l)==I(k)
-                ind = 0;
-                break;
+        [Y I] = min(d);
+        count = 0; % Number of non-overlapped correspondences
+        c1 = zeros(1,2); % Corresponding feature coordinates of im1
+        c2 = zeros(1,2); % Corresponding feature coordinates of im2
+          
+        % Euclidean distance will find a better match than just finding the
+        % nearest match. This will compare distances between the first and
+        % second nearest neighbour.
+        img = [imArray{n-1},imArray{n}];
+        for k = 1:length(Y)
+            ind = 1; % Indicator to avoid overlapped correspondences
+            for l = 1:length(I)
+                if l~=k && I(l)==I(k)
+                    ind = 0;
+                    break;
+                end
+            end
+            if ind && Y(k) < euclideanThresh % Threshold for Euclidean distance
+                count = count + 1;
+                c1(count,:) = round(F1(1:2,I(k)));
+                c2(count,:) = round(F2(1:2,k));
             end
         end
-        if ind && Y(k) < 55 % Threshold for Euclidean distance
-            count = count + 1;
-            c1(count,:) = round(F1(1:2,I(k)));
-            c2(count,:) = round(F2(1:2,k));
-        end
-    end
-    %% RANSAC algorithm %%
-    nc = 6; % Number of correspondences used to find a homography
-    N = fix(log(1-.99)/log(1-(1-.1)^nc)); % Number of trials by 10% rule
-    M = fix((1-.1)*count); % Minimum size for the inlier set
-    d_min = 1e100;
-    for o = 1:N
-        lcv = 1; % Loop control variable
-        while lcv % To avoid repeated selection
-            r = randi(count,nc,1);
-            r = sort(r);
-            for k = 1:nc-1
-                lcv = lcv*(r(k+1)-r(k));
+        %% RANSAC algorithm
+        % Ransac computes a homography matrix that can then be used to map
+        % the coordinates of one image to the coordinates of another image
+        nc = 6; % Number of correspondences used to find a homography
+        N = fix(log(1-.99)/log(1-(1-.1)^nc)); % Number of trials by 10% rule
+        M = fix((1-.1)*count); % Minimum size for the inlier set
+        d_min = 1e100;
+        for o = 1:N
+            lcv = 1; % Loop control variable
+            while lcv % To avoid repeated selection
+                r = randi(count,nc,1);
+                r = sort(r);
+                for k = 1:nc-1
+                    lcv = lcv*(r(k+1)-r(k));
+                end
+                lcv = ~lcv;
             end
-            lcv = ~lcv;
+            A = zeros(2*nc,9);
+            for k = 1:nc
+                A(2*k-1:2*k,:)=...
+                    [0,0,0,-[c1(r(k),:),1],c2(r(k),2)*[c1(r(k),:),1];
+                    [c1(r(k),:),1],0,0,0,-c2(r(k),1)*[c1(r(k),:),1]];
+            end
+            [U,D,V] = svd(A);
+            h = V(:,9);
+            H = [h(1),h(2),h(3);h(4),h(5),h(6);h(7),h(8),h(9)];
+
+            d2 = zeros(count,1); % d^2(x_measured, x_true)
+            for k = 1:count
+                x_true = H*[c1(k,:),1]'; % x_true in HC
+                temp = x_true/x_true(3);
+                x_true = temp(1:2); % x_true in image plane
+                d = c2(k,:)-x_true';
+                d2(k) = d(1)^2+d(2)^2;
+            end
+            [Y I] = sort(d2);
+            if sum(Y(1:M)) < d_min
+                d_min = sum(Y(1:M));
+                inliers = I(1:M);
+                outliers = I(M+1:end);
+            end
         end
-        A = zeros(2*nc,9);
-        for k = 1:nc
+
+        %% Linear Least Squares %%
+        A = zeros(2*M,9);
+        for k = 1:M
             A(2*k-1:2*k,:)=...
-                [0,0,0,-[c1(r(k),:),1],c2(r(k),2)*[c1(r(k),:),1];
-                [c1(r(k),:),1],0,0,0,-c2(r(k),1)*[c1(r(k),:),1]];
+                [0,0,0,-[c1(inliers(k),:),1],c2(inliers(k),2)*[c1(inliers(k),:),1];
+                [c1(inliers(k),:),1],0,0,0,-c2(inliers(k),1)*[c1(inliers(k),:),1]];
         end
         [U,D,V] = svd(A);
-        h = V(:,9);
-        H = [h(1),h(2),h(3);h(4),h(5),h(6);h(7),h(8),h(9)];
-    
-        d2 = zeros(count,1); % d^2(x_measured, x_true)
-        for k = 1:count
-            x_true = H*[c1(k,:),1]'; % x_true in HC
-            temp = x_true/x_true(3);
-            x_true = temp(1:2); % x_true in image plane
-            d = c2(k,:)-x_true';
-            d2(k) = d(1)^2+d(2)^2;
-        end
-        [Y I] = sort(d2);
-        if sum(Y(1:M)) < d_min
-            d_min = sum(Y(1:M));
-            inliers = I(1:M);
-            outliers = I(M+1:end);
-        end
-    end
+        h1 = V(:,9); % Homography estimated by LLS with all inliers
+        %% Non-linear Least Square (Levenberg-Marquardt) %%
+        c1 = c1(inliers,:)';
+        c1 = c1(:);
+        c2 = c2(inliers,:)';
+        c2 = c2(:);
+        opt = optimset('Algorithm','levenberg-marquardt');
+        h2 = lsqcurvefit(@fun,h1,c1,c2,[],[],opt); % Refined homography by L.M.
+        H = [h2(1),h2(2),h2(3);h2(4),h2(5),h2(6);h2(7),h2(8),h2(9)];
 
-    %% Linear Least Squares %%
-    A = zeros(2*M,9);
-    for k = 1:M
-        A(2*k-1:2*k,:)=...
-            [0,0,0,-[c1(inliers(k),:),1],c2(inliers(k),2)*[c1(inliers(k),:),1];
-            [c1(inliers(k),:),1],0,0,0,-c2(inliers(k),1)*[c1(inliers(k),:),1]];
-    end
-    [U,D,V] = svd(A);
-    h1 = V(:,9); % Homography estimated by LLS with all inliers
-    %% Non-linear Least Square (Levenberg-Marquardt) %%
-    c1 = c1(inliers,:)';
-    c1 = c1(:);
-    c2 = c2(inliers,:)';
-    c2 = c2(:);
-    opt = optimset('Algorithm','levenberg-marquardt');
-    h2 = lsqcurvefit(@fun,h1,c1,c2,[],[],opt); % Refined homography by L.M.
-    H = [h2(1),h2(2),h2(3);h2(4),h2(5),h2(6);h2(7),h2(8),h2(9)];
-        
-        fprintf('Saving H matrix for im%d and im%d\n',m-1, m);
-        
-        %name = ['H', string(m-1), '_', string(m)];
-        s = sprintf('%d', m-1);
-        name = ['homography/H', s];
-        save(char(name) , 'H');
-        
-        if (m > numToStitch/2)
-            m = m - 1;
-        else
-            m = m + 1;
-        end
+            fprintf('Saving H matrix for im%d and im%d\n',m-1, m);
+
+            %name = ['H', string(m-1), '_', string(m)];
+            s = sprintf('%d', m-1);
+            name = ['homography/H', s];
+            save(char(name) , 'H');
+
+            if (m > numToStitch/2)
+                m = m - 1;
+            else
+                m = m + 1;
+            end
                
 %         %% Showing inliner matches
 %         % again - for displaying points use the original im1 and im2 not Ig
@@ -256,6 +257,7 @@ for j = 1:2
     m=numToStitch; % m is used in the inner loop to keep track of which images to save
 end
 
+%some variables needed later
 [M,N,C] = size(imArray{2});
 
 fprintf('imreads done \n');
@@ -274,26 +276,29 @@ for n = 1:(numToStitch-1)
     HArray{n} = HArray{n}.H;
 end
 
-%  H12 = load('homography/H1           '); H12 = H12.H; % Homography of im1 to im2
-%  H23 = load('homography/H2           '); H23 = H23.H; % Homography of im3 to im2
-%  H34 = load('homography/H3           '); H34 = H34.H; % Homography of im1 to im2
-%  H54 = load('homography/H4           '); H54 = H54.H; % Homography of im3 to im2
-%  H65 = load('homography/H5           '); H65 = H65.H; % Homography of im1 to im2
-%  H76 = load('homography/H6           '); H76 = H76.H; % Homography of im3 to im2
-
 fprintf('Homography matrices loaded \n');
 
+%% Example homography order using 9 images (for easier visualisation)
+% This shows how to create a homography H to get from each image to the
+% centre image by multiplying the other homographies.
+% H15 represents the homography between images 1 and 5, and so forth.
+% im1 im2 im3 im4 im5 im6 im7 im8 im9 = order of images
+% H15 = H12*H23*H34*H45
+% H25 = H23*H34*H45
+% H35 = H34*H45
+% H45 - created in the loop earlier
+% H65 - created in the loop earlier
+% H75 = H76*H65
+% H85 = H87*H76*H65
+% H95 = H98*H87*H76*H65
 
-% % im1 im2 im3 im4 im5 im6 im7 <- order of single images : im4 is in center.
-% H14 = H12*H23*H34;
-% H24 = H23*H34;
-% H64 = H65*H54;
-% H74 = H76*H65*H54;
+%% HOMOGRAPHY CALCULATION
+% Using the above example as a template, this is performed on any number of
+% images using the below loops.
 
 % there is always 3 less homographies to create than numImages
-% 2 arrays are needed, one for the inwards, one for outwards
-%HxArray{1, (numToStitch-3)/2} = []; % creates an empty (1 x numToStich-3) cell array 
-HxArray = cell(1,(numToStitch-3)/2);
+% 2 arrays are needed, one for the inwards homographies, one for outwards homographies
+HxArray = cell(1,(numToStitch-3)/2); % creates an empty (1 x numToStich-3) cell array 
 HxArray2 = cell(1,(numToStitch-3)/2);
 
 %HxArray2{1, (numToStitch-3)/2} = []; % creates an empty (1 x numToStich-3) cell array 
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ImageStitching
-Contains matlab scripts and other pieces/code for my third year image stitching project
+# ImageStitching Instructions
+To run this program, the easiest way is to open Matlab and set the "MATLAB" folder as the current directory.
+Before running the program, the VLFeat Matlab toolbox must be installed (for the use of the VLFeat implementation of the SIFT algorithm)
+The VLFeat Toolbox has been included inside the "MATLAB" folder, so to add the toolbox to the MATLAB search path you just need to run the
+following command in the MATLAB command window: "run vlfeat-0.9.20\toolbox\vl_setup".
+
+Note: There is sometimes a bug where running the above command will give an error where the file path is listed as incorrect. Should this occur
+you will need to either try the full file path, or re-download the VLFeat binary package from "http://www.vlfeat.org/download.html",
+extract the files, and run the command using it's full file path.
